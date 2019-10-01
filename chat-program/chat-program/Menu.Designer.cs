a5800c5b1b6eb0626ade53d9025e4a4dd602bee1
Alex Chester <cheale14@coundoncourt.org>
2019-10-01 14:47:55 +0100

    initial

---
+++ chat-program/chat-program/Menu.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ChatProgram
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.btnHost = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(12, 12);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(177, 69);
+            this.btnJoin.TabIndex = 0;
+            this.btnJoin.Text = "Join a Text Server";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            // 
+            // btnHost
+            // 
+            this.btnHost.Location = new System.Drawing.Point(12, 87);
+            this.btnHost.Name = "btnHost";
+            this.btnHost.Size = new System.Drawing.Size(177, 69);
+            this.btnHost.TabIndex = 1;
+            this.btnHost.Text = "Host a Server";
+            this.btnHost.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 167);
+            this.Controls.Add(this.btnHost);
+            this.Controls.Add(this.btnJoin);
+            this.Name = "Menu";
+            this.Text = "Chat Program";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Button btnHost;
+    }
+}
+
---
+++ chat-program/chat-program/Server/ServerForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ChatProgram.Server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 403);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 421);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(566, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ServerForm";
+            this.Text = "ServerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
